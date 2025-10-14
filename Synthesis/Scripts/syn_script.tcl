0330c9dd9dac6b61368342a50c5bf014a6e069a2
Zeyad Elbarbary <167577132+zeyadelbarbary7@users.noreply.github.com>
2025-10-15 02:08:42 +0300

    Create syn_script.tcl

---
+++ Synthesis/Scripts/syn_script.tcl
@@ -0,0 +1,113 @@
+
+########################### Define Top Module ############################
+                                                   
+set top_module SYS_TOP
+
+##################### Define Working Library Directory ######################
+                                                   
+define_design_lib work -path ./work
+
+############################# Formality Setup File ##########################
+                                                   
+set_svf $top_module.svf
+
+################## Design Compiler Library Files #setup ######################
+
+puts "###########################################"
+puts "#      #setting Design Libraries          #"
+puts "###########################################"
+
+#Add the path of the libraries to the search_path variable
+lappend search_path /home/IC/Projects/System/std_cells
+lappend search_path /home/IC/Projects/System/rtl
+
+set SSLIB "scmetro_tsmc_cl013g_rvt_ss_1p08v_125c.db"
+set TTLIB "scmetro_tsmc_cl013g_rvt_tt_1p2v_25c.db"
+set FFLIB "scmetro_tsmc_cl013g_rvt_ff_1p32v_m40c.db"
+
+## Standard Cell libraries 
+set target_library [list $SSLIB $TTLIB $FFLIB]
+
+## Standard Cell & Hard Macros libraries 
+set link_library [list * $SSLIB $TTLIB $FFLIB]  
+
+######################## Reading RTL Files #################################
+
+puts "###########################################"
+puts "#             Reading RTL Files           #"
+puts "###########################################"
+
+set file_format verilog
+
+#used analyze and elaborate cause not all files got read by read_file
+analyze -format verilog [glob /home/IC/Projects/System/rtl/*.v]
+analyze -format sverilog [glob /home/IC/Projects/System/rtl/*.sv]
+elaborate -lib work SYS_TOP
+
+###################### Defining toplevel ###################################
+
+current_design $top_module
+
+#################### Liniking All The Design Parts #########################
+puts "###############################################"
+puts "######## Liniking All The Design Parts ########"
+puts "###############################################"
+
+link 
+
+#################### Liniking All The Design Parts #########################
+puts "###############################################"
+puts "######## checking design consistency ##########"
+puts "###############################################"
+
+check_design
+
+############################### Path groups ################################
+puts "###############################################"
+puts "################ Path groups ##################"
+puts "###############################################"
+
+group_path -name INREG -from [all_inputs]
+group_path -name REGOUT -to [all_outputs]
+group_path -name INOUT -from [all_inputs] -to [all_outputs]
+
+#################### Define Design Constraints #########################
+puts "###############################################"
+puts "############ Design Constraints #### ##########"
+puts "###############################################"
+
+source -echo ./cons.tcl
+
+###################### Mapping and optimization ########################
+puts "###############################################"
+puts "########## Mapping & Optimization #############"
+puts "###############################################"
+
+compile -map_effort high
+
+##################### Close Formality Setup file ###########################
+
+set_svf -off
+
+#############################################################################
+# Write out files
+#############################################################################
+write_file -format verilog -hierarchy -output $top_module.v
+write_file -format ddc -hierarchy -output $top_module.ddc
+write_sdc  -nosplit $top_module.sdc
+write_sdf           $top_module.sdf
+
+################# reporting #######################
+
+report_area -hierarchy > area.rpt
+report_power -hierarchy > power.rpt
+report_timing -max_paths 100 -delay_type min > hold.rpt
+report_timing -max_paths 100 -delay_type max > setup.rpt
+report_clock -attributes > clocks.rpt
+report_constraint -all_violators > constraints.rpt
+
+################# starting graphical user interface #######################
+
+#gui_start
+
+#exit
